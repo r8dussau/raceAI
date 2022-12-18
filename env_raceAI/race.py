8fbe3c2c1915a5fae105cdc3e55319cda9efb709
r8dussau <r8dussau@enib.fr>
2022-12-19 00:19:06 +0200

    resize images

--- env_raceAI/race.py
+++ env_raceAI/race.py
@@ -1,15 +1,16 @@
 import pygame
 import time
 import math
+from utils import scale_image
 
-GRASS = pygame.image.load("env_raceAI/imgs/grass.jpg")
-BORDER = pygame.image.load("env_raceAI/imgs/border_1.png")
-TRACK = pygame.image.load("env_raceAI/imgs/track_1.png")
+TRACK = scale_image(pygame.image.load("env_raceAI/imgs/track_1.png"),1.3)
+BORDER = scale_image(pygame.image.load("env_raceAI/imgs/border_1.png"),1.3)
+GRASS = scale_image(pygame.image.load("env_raceAI/imgs/grass.jpg"),500*1.3/311)
 FINISH = pygame.image.load("env_raceAI/imgs/finish.png")
-CAR_GREEN = pygame.image.load("env_raceAI/imgs/car_green.png")
+CAR_GREEN = scale_image(pygame.image.load("env_raceAI/imgs/car_green.png"),0.04)
 CAR_PURPLE = pygame.image.load("env_raceAI/imgs/car_purple.png")
 
-WIDTH, HEIGHT = GRASS.get_width()*1.5, GRASS.get_height()*1.5
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
 
@@ -22,7 +23,7 @@ while run:
 
     WIN.blit(GRASS, (0, 0)) 
     WIN.blit(TRACK, (0, 0))
-    WIN.blit(CAR_PURPLE, (0, 0))
+    WIN.blit(CAR_GREEN, (50, 150))
 
     pygame.display.update()
 
