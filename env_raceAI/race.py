982b6738039e276c305881bbb359b2678bc70ada
r8dussau <r8dussau@enib.fr>
2022-12-18 22:06:52 +0200

    commit n1

--- env_raceAI/race.py
+++ env_raceAI/race.py
@@ -1,1 +1,27 @@
-a = 5
+import pygame
+import time
+import math
+
+PROFIL = pygame.image.load("env_raceAI/imgs/profil.jpg")
+
+WIDTH, HEIGHT = PROFIL.get_width()/2, PROFIL.get_height()/4
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Racing Game!")
+
+
+run = True
+clock = pygame.time.Clock()
+
+while run:
+    clock.tick(60) #FPS
+
+    WIN.blit(PROFIL, (0, 0)) #show image 
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+    
+pygame.quit()
