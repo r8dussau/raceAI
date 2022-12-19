f4fee94417d2b588925ec5b533f0e06f6d53f7d8
r8dussau <r8dussau@enib.fr>
2022-12-19 18:15:00 +0200

    Display Text

--- env_raceAI/race.py
+++ env_raceAI/race.py
@@ -4,7 +4,9 @@
 import pygame
 import time
 import math
-from utils import scale_image, car_rotate_center
+from utils import scale_image, car_rotate_center, text_center
+
+pygame.font.init()
 
 #-----------------------------------------------------------------------------
 #image loading
@@ -25,9 +27,45 @@ WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
 
+MAIN_FONT = pygame.font.SysFont("comicsans", 30)
+INFO_FONT = pygame.font.SysFont("comicsans", 15)
+
+#-----------------------------------------------------------------------------
+#class area
+
+#class of the game info
+class GameInfo:
+    #function to initialize all parameters of the game info class 
+    def __init__(self, stage=1):
+        self.stage = stage
+        self.started = False
+        self.stage_start_time = 0
+
+    #function to increment the generation
+    def next_stage(self):
+        self.stage += 1
+        self.started = False
+
+    #function to reset 
+    def reset(self):
+        self.stage = 1
+        self.started = False
+        self.stage_start_time = 0
+
+    #function to start the time
+    def start_stage(self):
+        self.started = True
+        self.stage_start_time = time.time()
+
+    #function to get the time 
+    def get_stage_time(self):
+        if not self.started:
+            return 0
+        return int(time.time() - self.stage_start_time)
+
 #class of the car
 class Car:
-    #function ton initialize all parameters
+    #function to initialize all parameters of the car class 
     def __init__(self, max_vel, rot_vel):
         self.img = self.IMG
         self.max_vel = max_vel
@@ -69,6 +107,7 @@ class Car:
         self.x -= horizontal
         self.y -= vertical
 
+    #function to return a no none result if a car collide with the border track
     def collision(self, mask, x=0, y=0):
         car_mask = pygame.mask.from_surface(self.img)
         offset = (int(self.x - x), int(self.y - y))
@@ -80,12 +119,27 @@ class PlayerCar(Car):
     IMG = CAR_GREEN
     START_POS = (300, 660) #300, 585
 
-#function to display a large number of images
-def draw(win, imgs, plr_car):
+#-----------------------------------------------------------------------------
+#display and move function
+
+#function to display images and texts
+def draw(win, imgs, plr_car,gm_inf):
     for img, pos in imgs:
         win.blit(img, pos)
 
-    player_car.draw(win)    #draw the player's car after rotating
+    stage_text = MAIN_FONT.render(f"Generation: {game_info.stage}", 1, (255, 255, 255))
+    win.blit(stage_text, (10, 0))
+
+    time_text = MAIN_FONT.render(f"Time: {game_info.get_stage_time()}s", 1, (255, 255, 255))
+    win.blit(time_text, (10, 30))
+
+    info_text1 = INFO_FONT.render("Press the key: UP to go forward, DOWN to go back, RIGHT to go right, LEFT to go left", 1, (255, 255, 255))
+    win.blit(info_text1, (10, HEIGHT - info_text1.get_height()-30))
+
+    info_text2 = INFO_FONT.render("Select the parents of the next generation by clicking on them and then press ENTER", 1, (255, 255, 255))
+    win.blit(info_text2, (10, HEIGHT - info_text2.get_height()-10))
+
+    player_car.draw(win)
     pygame.display.update()
 
 #function to move the car's image
@@ -94,32 +148,45 @@ def move_player(plr_car,collision=False):
 
     #action in function of wich key was pressed
     if not collision:
-        if keys[pygame.K_q] or keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT]:
             player_car.rotate(left=True)
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT]:
             player_car.rotate(right=True)
-        if keys[pygame.K_z] or keys[pygame.K_UP]:
+        if keys[pygame.K_UP]:
             player_car.move_forward()
-        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN]:
             player_car.move_backward()
+
+#-----------------------------------------------------------------------------
 
 run = True
 clock = pygame.time.Clock()
 imgs = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, (283, 649))]
 player_car = PlayerCar(1.5, 4)
+game_info = GameInfo()
 
 while run:
     clock.tick(60)  #FPS
 
-    draw(WIN,imgs, player_car)   
+    draw(WIN,imgs, player_car, game_info) 
+
+    while not game_info.started:
+        text_center(WIN, MAIN_FONT, f"Press any key to start the stage {game_info.stage}!")
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                break
+
+            if event.type == pygame.KEYDOWN:
+                game_info.start_stage()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
-    
-    #move_player(player_car,colli)
-    
+
+    #stop the car if there is a collision between the track border and the car
     if player_car.collision(BORDER_MASK) == None:
         colli = False
         move_player(player_car,colli)
--- env_raceAI/utils.py
+++ env_raceAI/utils.py
@@ -10,3 +10,8 @@ def car_rotate_center(win, img, top_left, angle):
     rotated_img = pygame.transform.rotate(img, angle)                               #rotate around the top-left 
     new_rect = rotated_img.get_rect(center=img.get_rect(topleft=top_left).center)   #rotate around the center according to the rotation of the top-left
     win.blit(rotated_img, new_rect.topleft)                                         #display the car with the rotation
+
+#function to print the text in the middle of the window
+def text_center(win, font, text):
+    render = font.render(text, 1, (200, 200, 200))
+    win.blit(render, (win.get_width()/2 - render.get_width()/2, win.get_height()/2 - render.get_height()/2))
