0167dacf6f73e07dfc7c26300745f9b41862d2cb
r8dussau <r8dussau@enib.fr>
2023-01-04 17:58:13 +0200

    Line with calcul of distance

--- env_raceAI/main.py
+++ env_raceAI/main.py
@@ -4,7 +4,8 @@
 import pygame
 import time
 import math
-from utils import scale_image, car_rotate_center, text_center
+import numpy as np
+from utils import scale_image, car_rotate_center, text_center, distance, movePoint
 
 pygame.font.init()
 
@@ -14,6 +15,7 @@ pygame.font.init()
 TRACK = scale_image(pygame.image.load("env_raceAI/imgs/track_1.png"),1.7)#1.7
 GRASS = scale_image(pygame.image.load("env_raceAI/imgs/grass.jpg"),500*1.7/311)
 FINISH = scale_image(pygame.image.load("env_raceAI/imgs/finish.png"),0.475)
+
 
 BORDER = scale_image(pygame.image.load("env_raceAI/imgs/border_1.png"),1.7)
 BORDER_MASK = pygame.mask.from_surface(BORDER)  #create a mask of the border track for the collision 
@@ -25,11 +27,21 @@ CAR_PURPLE = pygame.image.load("env_raceAI/imgs/car_purple.png")
 #display on the screen area
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
-WIN = pygame.display.set_mode((WIDTH, 800)) #HEIGHT
+WIN = pygame.display.set_mode((WIDTH, 780)) #HEIGHT
 pygame.display.set_caption("Racing Game with Genetic Algorithm!")
 
 MAIN_FONT = pygame.font.SysFont("comicsans", 30)
 INFO_FONT = pygame.font.SysFont("comicsans", 15)
+
+WHITE = (255,255,255)
+GREEN = (0, 255, 0) 
+BLUE = (0, 0, 128)  
+BLACK = (1,1,1,255)
+
+GREY_TRACK = (110,111,114,255)
+GREY_TRACK2= (93,92,94,255)
+
+COLOR_LINE = (255, 0, 0)
 
 #-----------------------------------------------------------------------------
 #class area
@@ -76,6 +88,28 @@ class Car:
         self.x, self.y = self.START_POS
         self.acc = 0.05
 
+        self.mask = pygame.mask.from_surface(self.img)
+        self.rect = (0, 0)
+
+        self.width = 320
+        self.height = 651
+        self.center = (0, 0)
+
+        #Show lines of the car if it's True
+        self.showlines = False
+
+        self.leftCorner = (0, 0)
+        self.rightCorner = (0, 0)
+        self.ahead = (0, 0)
+
+        self.distLeft = 0
+        self.distRight = 0
+        self.distAhead = 0
+
+        self.input = np.array([[self.distLeft], [self.distRight], [self.distAhead]])
+        self.output = np.array([[0], [0]])
+        
+
     #function to rotate the car 
     def rotate(self, left=False, right=False):
         if left:
@@ -83,17 +117,49 @@ class Car:
         elif right:
             self.angle -= self.rot_vel
 
-    #function to call the car_rotate_center function with the arguments of the car class
+    #function to call the car_rotate_center function with the arguments of the car class + refresh self.rect and self.mask (for the collision)
     def draw(self, win):
-        self.mask = car_rotate_center(win, self.img, (self.x, self.y), self.angle)
+        self.rect, self.mask = car_rotate_center(win, self.img, (self.x, self.y), self.angle)       
+
+        if self.showlines:
+            pygame.draw.line(win,COLOR_LINE,self.center,self.rightCorner,2)
+            pygame.draw.line(win,COLOR_LINE,self.center,self.leftCorner,2) 
+            pygame.draw.line(win,COLOR_LINE,self.center,self.ahead,2)
+
+    def showLines(self):
+        self.showlines = not self.showlines
 
     #function to modify the velocity of the car to go forward
     def move_forward(self):
-        """ if self.vel < 0:
-            self.vel = 0 """
-        #self.vel = min(self.vel + self.acc, self.max_vel)
-        self.vel = self.max_vel
+        self.vel = min(self.vel + self.acc, self.max_vel)
         self.move()
+
+        self.center = (self.x + 0.5*round(self.width*0.04), self.y + 0.5*round(self.height*0.04)) #modified the center of the car when rotation
+
+        self.leftCorner = movePoint((self.x, self.y),self.angle+30,1.5) #1
+        while TRACK.get_at((int(self.leftCorner[0]),int(self.leftCorner[1]))) == GREY_TRACK:
+            self.leftCorner = movePoint((self.leftCorner[0],self.leftCorner[1]),self.angle+30,1.5) #0.5
+        while TRACK.get_at((int(self.leftCorner[0]),int(self.leftCorner[1]))) == GREY_TRACK2 or TRACK.get_at((int(self.leftCorner[0]),int(self.leftCorner[1]))) ==  BLACK:
+        #while TRACK.get_at((int(self.leftCorner[0]),int(self.leftCorner[1]))) != GREY_TRACK:
+            self.leftCorner = movePoint((self.leftCorner[0],self.leftCorner[1]),self.angle+30,-0.5) 
+
+        self.rightCorner = movePoint((self.x, self.y),self.angle-30,1.5) #1
+        while TRACK.get_at((int(self.rightCorner[0]),int(self.rightCorner[1]))) == GREY_TRACK:
+            self.rightCorner = movePoint((self.rightCorner[0],self.rightCorner[1]),self.angle-30,1.5)#0.5
+        while TRACK.get_at((int(self.rightCorner[0]),int(self.rightCorner[1]))) == GREY_TRACK2 or TRACK.get_at((int(self.rightCorner[0]),int(self.rightCorner[1]))) ==  BLACK:
+        #while TRACK.get_at((int(self.rightCorner[0]),int(self.rightCorner[1]))) != GREY_TRACK:
+            self.rightCorner = movePoint((self.rightCorner[0],self.rightCorner[1]),self.angle-30,-0.5)
+
+        self.ahead = movePoint((self.x, self.y),self.angle,1.5) #1
+        while TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) == GREY_TRACK:
+            self.ahead = movePoint((self.ahead[0],self.ahead[1]),self.angle,1.5)#0.5
+        while TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) == GREY_TRACK2 or TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) ==  BLACK:
+        #while TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) != GREY_TRACK:
+            self.ahead = movePoint((self.ahead[0],self.ahead[1]),self.angle,-0.5)
+
+        self.distAhead = int(distance(self.center, self.ahead)) 
+        self.distLeft = int(distance(self.center, self.leftCorner))
+        self.distRight = int(distance(self.center, self.rightCorner)) 
 
     #function to modify the velocity of the car to go backward
     """ def move_backward(self):
@@ -107,17 +173,12 @@ class Car:
         horizontal = math.sin(radians) * self.vel 
 
         self.x -= horizontal
-        self.y -= vertical
-
+        self.y -= vertical 
+    
     #function to return a no none result if a car collide with the border track
     def collision(self, mask, x=0, y=0):
-        
-        rotated_img = pygame.transform.rotate(self.img, self.angle)                            
-        new_rect = rotated_img.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
-
-        car_mask = pygame.mask.from_surface(rotated_img)
-        offset = (int(new_rect.x - x), int(new_rect.y - y)) 
-        col = mask.overlap(car_mask, offset) 
+        offset = (int(self.rect.x - x), int(self.rect.y - y)) 
+        col = mask.overlap(self.mask, offset) 
         return col
 
     def reset(self):
@@ -125,10 +186,13 @@ class Car:
         self.angle = 90
         self.vel = 0
 
+
 #class of the player's car
 class PlayerCar(Car):
     IMG = CAR_GREEN
-    START_POS = (300, 660) 
+    START_POS = (300, 660)
+
+    #MASK = pygame.mask.from_surface(CAR_GREEN) 
 
 #-----------------------------------------------------------------------------
 #display and move function
@@ -145,10 +209,10 @@ def draw(win, imgs, plr_car,gm_inf):
     win.blit(time_text, (10, 30))
 
     info_text1 = INFO_FONT.render("RIGHT to turn right, LEFT to turn left", 1, (255, 255, 255))   #Press the key: UP to go forward, DOWN to go back, RIGHT to go right, LEFT to go left
-    win.blit(info_text1, (10, 800 - info_text1.get_height()-30)) #HEIGHT
+    win.blit(info_text1, (10, 780 - info_text1.get_height()-30)) #HEIGHT
 
     info_text2 = INFO_FONT.render("When all the car are crashed press ENTER", 1, (255, 255, 255)) #Select the parents of the next generation by clicking on them and then press ENTER
-    win.blit(info_text2, (10, 800 - info_text2.get_height()-10)) #HEIGHT
+    win.blit(info_text2, (10, 780 - info_text2.get_height()-10)) #HEIGHT
 
     player_car.draw(win)
     pygame.display.update()
@@ -163,6 +227,8 @@ def move_player(plr_car,collision=False):
             player_car.rotate(left=True)
         if keys[pygame.K_RIGHT]:
             player_car.rotate(right=True)
+        if keys[pygame.K_a]:
+            player_car.showLines()
         #if keys[pygame.K_UP]:
             #player_car.move_forward()
         player_car.move_forward()
@@ -181,7 +247,7 @@ def move_player(plr_car,collision=False):
 run = True
 clock = pygame.time.Clock()
 imgs = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, (283, 649))]
-player_car = PlayerCar(1.5, 4)
+player_car = PlayerCar(1.5, 4) #1.5
 game_info = GameInfo()
 
 #-----------------------------------------------------------------------------
--- env_raceAI/utils.py
+++ env_raceAI/utils.py
@@ -1,4 +1,6 @@
 import pygame 
+import math
+import numpy as np
 
 #function to reajust the size of the images
 def scale_image(img,factor):
@@ -9,13 +11,41 @@ def scale_image(img,factor):
 def car_rotate_center(win, img, top_left, angle):
     rotated_img = pygame.transform.rotate(img, angle)                               #rotate around the top-left 
     new_rect = rotated_img.get_rect(center=img.get_rect(topleft=top_left).center)   #center new rotated image on the center of original image
-    win.blit(rotated_img, new_rect.topleft)                                         #display the car with the rotation
-    
-    """ pygame.draw.rect(win, (255, 0, 0), new_rect, 2) """  #draw rectangle of the image
-    """ car_mask = pygame.mask.from_surface(img).to_surface()
-    pygame.draw.rect(win, (0, 255, 0), car_mask.get_rect(center=img.get_rect(topleft=top_left).center), 2) """ #draw the rectangle if mask isn't rotated when checking collision                                      
+    win.blit(rotated_img, new_rect.topleft)                                          #display the car with the rotation
+
+    """ pygame.draw.rect(win, (255, 0, 0), new_rect, 2)   #draw rectangle of the image
+    car_mask = pygame.mask.from_surface(img).to_surface()
+    pygame.draw.rect(win, (0, 255, 0), car_mask.get_rect(center=img.get_rect(topleft=top_left).center), 2)  #draw the rectangle if mask isn't rotated when checking collision """                                      
+
+    return new_rect, pygame.mask.from_surface(rotated_img)
 
 #function to print the text in the middle of the window
 def text_center(win, font, text):
     render = font.render(text, 1, (200, 200, 200))
     win.blit(render, (win.get_width()/2 - render.get_width()/2, win.get_height()/2 - render.get_height()/2))
+
+def movePoint(point,angle,velo):
+        x = point[0]
+        y = point[1]
+
+        radians = math.radians(angle%360)
+        vertical = math.cos(radians) * velo
+        horizontal = math.sin(radians) * velo
+
+        x -= horizontal
+        y -= vertical
+
+        return x, y
+
+def distance(center, end):
+        return math.sqrt((end[0]-center[0])**2 + (end[1]-center[1])**2)
+
+""" def rotation(origin, point, angle): #Used to rotate points #rotate(origin, point, math.radians(10))
+    ox, oy = origin
+    px, py = point
+    angle = -angle
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+   
+    return qx, qy  """
