bf0b83a8206aa43d2eff6feba94b8e741e1b716d
r8dussau <r8dussau@enib.fr>
2023-02-08 18:12:21 +0200

    Final Version

---
+++ env_raceAI/IAFunction.py
@@ -0,0 +1,75 @@
+from random import randint
+import pygame
+
+
+#-----------------------------------------------------------------------------
+#function to display every car controlled by the AI
+def draw_car(win, aiCars):
+    for aicar in aiCars:
+        aicar.draw(win)
+        pygame.display.update()
+
+#function to move the aiCar randomly or create a childrenList according to the decisionList of the two parents
+def move_ai(aicar):
+    if len(aicar.decisionList) == 0:
+        decision = randint(0,2)
+        aicar.decisionList.append(decision)
+    elif (len(aicar.decisionList) != 0) and (aicar.iteration != len(aicar.decisionList)-1):
+        decision=aicar.decisionList[aicar.iteration]
+        aicar.iteration += 1
+    else:   
+        decision = randint(0,2)
+        aicar.decisionList.append(decision)
+
+    if not aicar.collide:
+        aicar.move_forward()
+        if decision==0:
+            aicar.rotate(left=True)
+        elif decision==1:
+            aicar.rotate(right=True)
+        else:
+            pass
+
+#-----------------------------------------------------------------------------
+#Genetic Algorithm function
+
+#get the fitness value of each car controlled by the AI
+def fitness(aicar):
+    aicar.fitnessValue = 2000 - aicar.distTraveled + aicar.numbOfTurn/2
+
+#create the list of the father and the mother according to the car which is parentOne or parentTwo
+#According to these two parents list, create for each children a list who each cell has the same probability to have the cell of the mother or the father
+def crossover(aiCars):#aicar
+    father, mother = list(), list()
+    for aicar in aiCars:
+        if aicar.parentOne==True:
+            father = aicar.decisionList[:len(aicar.decisionList)-3]
+            aicar.decisionList = father
+
+        elif aicar.parentTwo==True:
+            mother = aicar.decisionList[:len(aicar.decisionList)-3]
+            aicar.decisionList = mother
+
+    for aicar in aiCars:
+        if (aicar.parentOne==False) and (aicar.parentTwo==False):
+            aicar.decisionList = list()
+            for i in range(len(father)):
+                random = randint(1,2)
+                if random == 1:
+                    aicar.decisionList.append(father[i])
+                elif (random == 2) and (i<len(mother)):
+                    aicar.decisionList.append(mother[i])
+                else:
+                    aicar.decisionList.append(father[i])
+    
+#each cell of each list has 0.4% to see his cell randomly changed
+def mutation(aiCars):
+    for aicar in aiCars:
+        for i in range(len(aicar.decisionList)):
+                random = randint(1,2000) #0.05%
+                if random==1:
+                    aicar.decisionList[i]=randint(0,2)
+    
+
+
+
--- env_raceAI/main.py
+++ env_raceAI/main.py
@@ -6,6 +6,7 @@ import time
 import math
 from random import randint
 from utils import scale_image, car_rotate_center, text_center, distance, movePoint, rotation
+from IAFunction import move_ai, draw_car, fitness, crossover, mutation
 
 pygame.font.init()
 
@@ -29,19 +30,6 @@ CAR_PURPLE = scale_image(pygame.image.load("env_raceAI/imgs/car_purple.png"),0.0
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, 780)) #HEIGHT
 pygame.display.set_caption("Racing Game with Genetic Algorithm!")
-
-MAIN_FONT = pygame.font.SysFont("comicsans", 30)
-INFO_FONT = pygame.font.SysFont("comicsans", 15)
-
-WHITE = (255,255,255)
-GREEN = (0, 255, 0) 
-BLUE = (0, 0, 128)  
-BLACK = (1,1,1,255)
-
-GREY_TRACK = (110,111,114,255)
-GREY_TRACK2= (93,92,94,255)
-
-COLOR_LINE = (255, 0, 0)
 
 #-----------------------------------------------------------------------------
 #class area
@@ -165,7 +153,7 @@ class Car:
 
     #for the player_car
     def update(self):
-        self.center = (self.x + 0.5*round(self.width*0.04), self.y + 0.5*round(self.height*0.04)) #modified the center of the car when rotation
+        self.center = (self.x + 0.5*round(self.width*0.04), self.y + 0.5*round(self.height*0.04)) 
 
         self.topLeft = (self.center[0] - 0.04*self.width/2, self.center[1] + 0.04*self.height/2)
         self.topRight = (self.center[0] + 0.04*self.width/2, self.center[1] + 0.04*self.height/2)
@@ -179,23 +167,20 @@ class Car:
 
         self.left = movePoint((self.x, self.y),self.angle+30,1.5) #1
         while TRACK.get_at((int(self.left[0]),int(self.left[1]))) == GREY_TRACK:
-            self.left = movePoint((self.left[0],self.left[1]),self.angle+30,1.5) #0.5
+            self.left = movePoint((self.left[0],self.left[1]),self.angle+30,1.5) #0.5s
         while TRACK.get_at((int(self.left[0]),int(self.left[1]))) == GREY_TRACK2 or TRACK.get_at((int(self.left[0]),int(self.left[1]))) ==  BLACK:
-        #while TRACK.get_at((int(self.left[0]),int(self.left[1]))) != GREY_TRACK:
             self.left = movePoint((self.left[0],self.left[1]),self.angle+30,-0.5) 
 
         self.right = movePoint((self.x, self.y),self.angle-30,1.5) #1
         while TRACK.get_at((int(self.right[0]),int(self.right[1]))) == GREY_TRACK:
             self.right = movePoint((self.right[0],self.right[1]),self.angle-30,1.5)#0.5
         while TRACK.get_at((int(self.right[0]),int(self.right[1]))) == GREY_TRACK2 or TRACK.get_at((int(self.right[0]),int(self.right[1]))) ==  BLACK:
-        #while TRACK.get_at((int(self.right[0]),int(self.right[1]))) != GREY_TRACK:
             self.right = movePoint((self.right[0],self.right[1]),self.angle-30,-0.5)
 
         self.ahead = movePoint((self.x, self.y),self.angle,1.5) #1
         while TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) == GREY_TRACK:
             self.ahead = movePoint((self.ahead[0],self.ahead[1]),self.angle,1.5)#0.5
         while TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) == GREY_TRACK2 or TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) ==  BLACK:
-        #while TRACK.get_at((int(self.ahead[0]),int(self.ahead[1]))) != GREY_TRACK:
             self.ahead = movePoint((self.ahead[0],self.ahead[1]),self.angle,-0.5)
 
         self.distAhead = int(distance(self.center, self.ahead)) 
@@ -217,11 +202,7 @@ class Car:
         col = mask.overlap(self.mask, offset) 
         return col
 
-    #
-    def calcul(self):
-        return self.distTraveled-self.numbOfTurn
-    #
-
+    #reset the value of the car
     def reset(self):
         self.x, self.y = self.START_POS
         self.angle = 90
@@ -233,20 +214,15 @@ class Car:
         self.iteration = 0
 
 
-#class of the player's car
+#class of the car with the initial color and position
 class PlayerCar(Car):
     IMG = CAR_GREEN
     START_POS = (300, 660)
 
 
 #-----------------------------------------------------------------------------
-#display and move function
 
-#function to display images and texts
-def draw_car(win, car):
-    car.draw(win)
-    pygame.display.update()
-
+#display the screen with the images and the informations
 def draw_background(win,imgs, gm_inf):
     for img, pos in imgs:
         win.blit(img, pos)
@@ -284,72 +260,21 @@ def move_player(plr_car,keys):
         plr_car.update()
     pygame.display.update()
 
-#function to move the aiCar randomly or according to the decisionList, built by the parents
-def move_ai(aicar):
-    if len(aicar.decisionList) == 0:
-        decision = randint(0,2)
-        aicar.decisionList.append(decision)
-    elif (len(aicar.decisionList) != 0) and (aicar.iteration != len(aicar.decisionList)-1):
-        decision=aicar.decisionList[aicar.iteration]
-        aicar.iteration += 1
-    else:   
-        decision = randint(0,2)
-        aicar.decisionList.append(decision)
-
-    if not aicar.collide:
-        aicar.move_forward()
-        if decision==0:
-            aicar.rotate(left=True)
-        elif decision==1:
-            aicar.rotate(right=True)
-        else:
-            pass
-
-    #pygame.display.update()
-        
-#-----------------------------------------------------------------------------
-#Genetic Algorithm function
-
-#get the fitness value of each car controlled by the AI
-def fitness(aicar):
-    aicar.fitnessValue = 2000 - aicar.distTraveled + aicar.numbOfTurn/2
-
-#create the list of the father and the mother according to the car which is parentOne or parentTwo
-#According to these two parents list, create for each children a list who each cell has the same probability to have the cell of the mother or the father
-def crossover(aiCars):#aicar
-    father, mother = list(), list()
-    for aicar in aiCars:
-        if aicar.parentOne==True:
-            father = aicar.decisionList[:len(aicar.decisionList)-3]
-            aicar.decisionList = father
-
-        elif aicar.parentTwo==True:
-            mother = aicar.decisionList[:len(aicar.decisionList)-3]
-            aicar.decisionList = mother
-
-    for aicar in aiCars:
-        if (aicar.parentOne==False) and (aicar.parentTwo==False):
-            aicar.decisionList = list()
-            for i in range(len(father)):
-                random = randint(1,2)
-                if random == 1:
-                    aicar.decisionList.append(father[i])
-                elif (random == 2) and (i<len(mother)):
-                    aicar.decisionList.append(mother[i])
-                else:
-                    aicar.decisionList.append(father[i])
-    
-#each cell of each list has 0.4% to see his cell randomly changed
-def mutation(aiCars):
-    for aicar in aiCars:
-        for i in range(len(aicar.decisionList)):
-                random = randint(1,250) #0.5%
-                if random==1:
-                    aicar.decisionList[i]=randint(0,2)
-    
-
 #-----------------------------------------------------------------------------
 #initialization area
+
+MAIN_FONT = pygame.font.SysFont("comicsans", 30)
+INFO_FONT = pygame.font.SysFont("comicsans", 15)
+
+WHITE = (255,255,255)
+GREEN = (0, 255, 0) 
+BLUE = (0, 0, 128)  
+BLACK = (1,1,1,255)
+
+GREY_TRACK = (110,111,114,255)
+GREY_TRACK2= (93,92,94,255)
+
+COLOR_LINE = (255, 0, 0)
 
 run = True
 clock = pygame.time.Clock()
@@ -362,7 +287,7 @@ numbOfCar = 30
 aliveCar = numbOfCar
 
 for i in range(numbOfCar):
-    aiCars.append(PlayerCar(2.5,4)) #1.5
+    aiCars.append(PlayerCar(2.5,4))
 
 
 #-----------------------------------------------------------------------------
@@ -371,13 +296,9 @@ for i in range(numbOfCar):
 while run:
     clock.tick(60)  #FPS
 
-    #display the screen with the images and the informations
     draw_background(WIN,imgs,game_info)
 
-    #display all the car controlled by the ai
-    for aicar in aiCars:
-        draw_car(WIN, aicar)  
-    #draw_car(WIN, player_car)
+    draw_car(WIN, aiCars)
 
     #Display text before the begining of each generation 
     while not game_info.started:
@@ -443,19 +364,25 @@ while run:
             game_info.next_stage()
             aliveCar = numbOfCar
 
+    #-------------------------------------------------------------------------
+    #If you want to display and control a Car
 
+    #display the player_car
+    #draw_car(WIN, player_car)
 
     #control of the player's car
-    """ if player_car.collision(BORDER_MASK) == None:
-        player_car.collide = False
-        move_player(player_car,keys)
-    else:
-        player_car.collide = True
-        move_player(player_car,keys)
-        fitness(player_car)
+    # if player_car.collision(BORDER_MASK) == None:
+    #     player_car.collide = False
+    #     move_player(player_car,keys)
+    # else:
+    #     player_car.collide = True
+    #     move_player(player_car,keys)
+    #     fitness(player_car)
     
         
-    if keys[pygame.K_a]:
-        player_car.showLines() """ 
+    # if keys[pygame.K_a]:
+    #     player_car.showLines() 
+
+    #-------------------------------------------------------------------------
 
 pygame.quit() 
